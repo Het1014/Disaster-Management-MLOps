59aa167531ef4470b2b633acca76a5e4ca6df584
Het1014 <hetjbuch@gmail.com>
2026-02-07 16:22:25 +0530

    Added module to call ML container via container and not localhost

---
+++ api/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+import requests
+from config import ML_SERVICE_URL
+
+app = Flask(__name__)
+
+@app.route("/health", methods=["GET"])
+def health():
+    return {"status": "API service running"}, 200
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = request.get_json()
+
+    if not data or "message" not in data:
+        return jsonify({"error": "Missing 'message' field"}), 400
+
+    response = requests.post(
+        f"{ML_SERVICE_URL}/predict",
+        json={"message": data["message"]},
+        timeout=5
+    )
+
+    return jsonify(response.json()), response.status_code
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
